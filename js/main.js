02bf913f5357e08adcd2d2214215a312f9d13885
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2024-01-09 16:06:33 +0100

    Done event litseners on input fields and sliders.

--- js/main.js
+++ js/main.js
@@ -5,10 +5,7 @@ const ränta = document.querySelector("#ränta");
 const ränta_field = document.querySelector("#ränta-field");
 const amortering = document.querySelector("#amorteringstid");
 const amortering_field = document.querySelector("#amorteringstid-field");
-lånebelopp_field.value = lånebelopp.value;
-ränta_field.value = ränta.value;
-amortering_field.value = ränta.value;
-// Update the current slider value (each time you drag the slider handle)
+// Update the current slider values
 lånebelopp.oninput = () => {
     lånebelopp_field.value = lånebelopp.value;
 };
@@ -18,3 +15,44 @@ ränta.oninput = () => {
 amortering.oninput = () => {
     amortering_field.value = amortering.value;
 };
+lånebelopp_field.addEventListener("keydown", (event) => {
+    if (event.key === "Enter")
+        check_field(lånebelopp_field, lånebelopp);
+});
+lånebelopp_field.addEventListener("blur", () => check_field(lånebelopp_field, lånebelopp));
+ränta_field.addEventListener("keydown", (event) => {
+    if (event.key === "Enter")
+        check_field(ränta_field, ränta);
+});
+ränta_field.addEventListener("blur", () => check_field(ränta_field, ränta));
+amortering_field.addEventListener("keydown", (event) => {
+    if (event.key === "Enter")
+        check_field(amortering_field, amortering);
+});
+amortering_field.addEventListener("blur", () => check_field(amortering_field, amortering));
+function check_field(node, slider) {
+    if (is_valid_number(node.value)) {
+        node.classList.remove("failed");
+        slider.value = node.value;
+        if (validate())
+            calculate();
+    }
+    else
+        node.classList.add("failed");
+}
+function validate() {
+    if (is_valid_number(lånebelopp_field.value) &&
+        is_valid_number(ränta_field.value) &&
+        is_valid_number(amortering_field.value)) {
+        calculate();
+        return true;
+    }
+    else
+        return false;
+}
+function is_valid_number(value) {
+    return /^\d+(\.\d+)?$/.test(value);
+}
+function calculate() {
+    console.log("Calculations were conducted.");
+}
--- ts/main.ts
+++ ts/main.ts
@@ -11,11 +11,7 @@ const amortering_field = document.querySelector(
   "#amorteringstid-field"
 ) as HTMLInputElement;
 
-lånebelopp_field.value = lånebelopp.value;
-ränta_field.value = ränta.value;
-amortering_field.value = ränta.value;
-
-// Update the current slider value (each time you drag the slider handle)
+// Update the current slider values
 lånebelopp.oninput = () => {
   lånebelopp_field.value = lånebelopp.value;
 };
@@ -25,3 +21,49 @@ ränta.oninput = () => {
 amortering.oninput = () => {
   amortering_field.value = amortering.value;
 };
+
+lånebelopp_field.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") check_field(lånebelopp_field, lånebelopp);
+});
+lånebelopp_field.addEventListener("blur", () =>
+  check_field(lånebelopp_field, lånebelopp)
+);
+
+ränta_field.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") check_field(ränta_field, ränta);
+});
+ränta_field.addEventListener("blur", () => check_field(ränta_field, ränta));
+
+amortering_field.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") check_field(amortering_field, amortering);
+});
+amortering_field.addEventListener("blur", () =>
+  check_field(amortering_field, amortering)
+);
+
+function check_field(node: HTMLInputElement, slider: HTMLInputElement): void {
+  if (is_valid_number(node.value)) {
+    node.classList.remove("failed");
+    slider.value = node.value;
+    if (validate()) calculate();
+  } else node.classList.add("failed");
+}
+
+function validate(): boolean {
+  if (
+    is_valid_number(lånebelopp_field.value) &&
+    is_valid_number(ränta_field.value) &&
+    is_valid_number(amortering_field.value)
+  ) {
+    calculate();
+    return true;
+  } else return false;
+}
+
+function is_valid_number(value: string): boolean {
+  return /^\d+(\.\d+)?$/.test(value);
+}
+
+function calculate(): void {
+  console.log("Calculations were conducted.");
+}
